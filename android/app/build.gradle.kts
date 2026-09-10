20572465f744cd7c5a874295747d67dfcd2cfbc7
jhaago <jordanhaagensen@gmail.com>
2026-09-10 08:34:57 +0800

    Bump Android POC to V0.1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jordanhaagensen.youtubedownloader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
