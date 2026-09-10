7e449aabfa3c69a57ad0c55bed1ce52a6e33105e
jhaago <jordanhaagensen@gmail.com>
2026-09-10 16:26:25 +0800

    Bump Android app to V0.2.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jordanhaagensen.youtubedownloader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.1"
+        versionCode = 5
+        versionName = "0.2.2"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
