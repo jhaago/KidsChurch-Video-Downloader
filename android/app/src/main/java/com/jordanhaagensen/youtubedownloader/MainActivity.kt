a44050241afe1ab9c56d260e428c26c18fe23368
jhaago <jordanhaagensen@gmail.com>
2026-09-10 12:08:19 +0800

    Fix Compose weight usage in Android V0.2

--- android/app/src/main/java/com/jordanhaagensen/youtubedownloader/MainActivity.kt
+++ android/app/src/main/java/com/jordanhaagensen/youtubedownloader/MainActivity.kt
@@ -22,7 +22,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
